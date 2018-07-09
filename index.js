483a8f6cd3d70a043019f6e40eaf9d611889bcf2
AnRRm <32653579+AnRRm@users.noreply.github.com>
2018-07-09 20:39:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function getweatherpicture(description)
 function getGreetingReply(req, res) {
 	
 	var userName = req.body.conversation.participant_data.userName;
-	if(undifined == userName)
+	if(undefined  == userName)
 	{
 		userName = '';
 	}
